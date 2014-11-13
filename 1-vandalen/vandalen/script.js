f4b2f14e62562811e738e19da95f46988b2d21ff
ss223ck <ss223ck@student.lnu.se>
2014-11-13 21:09:38 +0000

    added some tests for the code

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -5,13 +5,24 @@ var makePerson = function(persArr){
     var arrayOfAge;
     var nameString = "";
     var equationForAverage = 0;
-    
+    var i = 0;
     var sendBackObject = {
         minAge:400,
         maxAge:0,
         averageAge:0,
         names:""
     };
+    
+    try 
+    {
+    for (i = 0; i < persArr.length; i+= 1)
+    {
+        if (typeof persArr[i].name !== 'string')
+        {
+            throw new Error("namn var inte av typen string");
+        }
+       
+    }
     
     arrayOfNames = persArr.map(function(personObject){return personObject.name});
     arrayOfNames.sort(String.localeCompare);
@@ -23,14 +34,19 @@ var makePerson = function(persArr){
     sendBackObject.minAge = arrayOfAge[0];
     sendBackObject.maxAge = arrayOfAge[arrayOfAge.length - 1];
     
-    for(var i = 0; i < arrayOfAge.length; i+= 1)
+    for(i = 0; i < arrayOfAge.length; i+= 1)
     {
         equationForAverage += arrayOfAge[i];
     }
     equationForAverage = Math.round(equationForAverage / arrayOfAge.length);
     
     sendBackObject.averageAge = equationForAverage;
-    
+    }
+    catch (error)
+    {
+        alert(error.message);
+    }
     return sendBackObject;
+    
 }
 
