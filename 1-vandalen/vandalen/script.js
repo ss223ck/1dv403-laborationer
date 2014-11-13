2baa06b113f2e2756183bcf9289cd7cf26c2354c
ss223ck <ss223ck@student.lnu.se>
2014-11-13 20:33:02 +0000

    passed more tests

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,7 +4,7 @@ var makePerson = function(persArr){
     var arrayOfNames;
     var arrayOfAge;
     var nameString = "";
-    var equationForAverage;
+    var equationForAverage = 0;
     
     var sendBackObject = {
         minAge:400,
@@ -18,13 +18,18 @@ var makePerson = function(persArr){
     
     arrayOfAge = persArr.map(function(personObject){return personObject.age});
     arrayOfAge.sort();
-    
-    equationForAverage = 
-    
+
     sendBackObject.names = arrayOfNames[0] + ", " + arrayOfNames[1] + ", " + arrayOfNames[2];
     sendBackObject.minAge = arrayOfAge[0];
-    sendBackObject.maxAge = arrayOfAge[arrayOfAge.length];
-    sendBackObject.averageAge = (arrayOfAge[0] + arrayOfAge[1] + arrayOfAge[2]) / 3;
+    sendBackObject.maxAge = arrayOfAge[arrayOfAge.length - 1];
+    
+    for(var i = 0; i < arrayOfAge.length; i+= 1)
+    {
+        equationForAverage += arrayOfAge[i];
+    }
+    equationForAverage = Math.round(equationForAverage / arrayOfAge.length);
+    
+    sendBackObject.averageAge = equationForAverage;
     
     return sendBackObject;
 }
