f14db343ed37dd204c4743a27b02c04f7785b63d
ss223ck <ss223ck@student.lnu.se>
2014-12-02 17:51:42 +0000

    tested Message.js

--- 2-labbymezzage/javascript/Message.js
+++ 2-labbymezzage/javascript/Message.js
@@ -4,29 +4,30 @@ function Message(message, date){
     
     this.getText = function() {
         return message;
-    }
+    };
     
     this.setText = function(_text){
         message = _text;
-    }
+    };
     
     this.getDate = function(){
         return date;
-    }
+    };
     
     this.setDate = function(_date){
         date = _date;
-    }
+    };
 }
 
 Message.prototype.toString = function() {
     return this.getText()+" ("+this.getDate()+") ";
-}
+};
 
 Message.prototype.getHTMLText = function(){
-    
-}
+    var text = this.getText();
+    text.replace(/[\n\r]/g,"<br />");
+};
 
 Message.prototype.getDateText = function(){
     
-}
+};
--- 2-labbymezzage/javascript/Messageboard.js
+++ 2-labbymezzage/javascript/Messageboard.js
@@ -1,8 +1,19 @@
 "use strict"
 
 
-var mess = new Message("testmedelande", new Date());
-alert(mess);
-alert(mess.getText());
-mess.setText("en annan text");
-alert(mess);
+
+var MessageBoard = {
+    
+    
+    message: [],
+    
+    init:function(){
+        var mess = new Message("ett medelanden", new Date());
+        alert(mess);
+        alert(mess.getText());
+        mess.setText("ett annat medelanden");
+        alert(mess);
+    }
+};
+
+window.addEventListener("load", MessageBoard.init);
