2f7e7adb95b338c768897a04c58c20738b75a79a
ss223ck <ss223ck@student.lnu.se>
2014-12-03 11:15:02 +0000

    halfway done with 6

--- 2-labbymezzage/javascript/Messageboard.js
+++ 2-labbymezzage/javascript/Messageboard.js
@@ -10,13 +10,25 @@ var MessageBoard = {
     init:function(){
         var buttonPressed = document.getElementById("buttonSend");
         
-        buttonPressed.addEventListener("click", function(e){
-            var node = this.parentNode;
-            var messageObject = new Message(node.msg.value, new Date());
-            MessageBoard.message.push(messageObject);
-            console.log(MessageBoard.message[0].getText());
-            e.preventDefault();
-        });
+        buttonPressed.addEventListener("click", MessageBoard.eventButtonPressed);
+    },
+    renderMessages: function(){
+        
+    },
+    renderMessage: function(messageID){
+        var nodeMessageArea = document.getElementById("messageArea");
+        var messageBox = document.createElement("div");
+        messageBox.setAttribute("MB-messageID", messageID);
+        messageBox.classList.add("messageBox");
+        nodeMessageArea.appendChild(messageBox);
+    },
+    
+    eventButtonPressed: function(e){
+        var node = this.parentNode;
+        var messageObject = new Message(node.msg.value, new Date());
+        MessageBoard.message.push(messageObject);
+        MessageBoard.renderMessage(MessageBoard.message.length - 1);
+        e.preventDefault();
     }
 };
 
