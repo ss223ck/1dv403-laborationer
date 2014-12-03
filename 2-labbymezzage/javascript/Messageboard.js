f2dfde930de211f2f073e9e588b2e33d859a4edb
ss223ck <ss223ck@student.lnu.se>
2014-12-03 14:15:00 +0000

    almoste done with 7

--- 2-labbymezzage/javascript/Messageboard.js
+++ 2-labbymezzage/javascript/Messageboard.js
@@ -9,11 +9,17 @@ var MessageBoard = {
     
     init:function(){
         var buttonPressed = document.getElementById("buttonSend");
-        
         buttonPressed.addEventListener("click", MessageBoard.eventButtonPressed);
+        
+        var imageClick = document.getElementById("messageArea");
+        imageClick.addEventListener("click", MessageBoard.eventCloseButtonPressed);
     },
     renderMessages: function(){
+        document.getElementById("messageArea").innerHTML = "";
         
+        for(var i = 0; i < MessageBoard.message.length; i+=1){
+            MessageBoard.renderMessage(i);
+        }
     },
     renderMessage: function(messageID){
         var nodeMessageArea = document.getElementById("messageArea");
@@ -21,10 +27,19 @@ var MessageBoard = {
         messageBox.setAttribute("MB-messageID", messageID);
         messageBox.classList.add("messageBox");
         nodeMessageArea.appendChild(messageBox);
-        //messageBox.innerHTML = MessageBoard.message[messageID].getText();
+        messageBox.innerHTML = MessageBoard.message[messageID].getText();
         //messagebox.nodeValue = MessageBoard.message[messageID].getText();   ???varför fungerar ej detta.
-        var textVariabel = document.createTextNode(MessageBoard.message[messageID].getText());
-        messageBox.appendChild(textVariabel);
+        //var textVariabel = document.createTextNode(MessageBoard.message[messageID].getText());
+        //messageBox.appendChild(textVariabel);
+        
+        var linkIconRemove = document.createElement("a");
+        var iconRemove = document.createElement("img");
+        iconRemove.classList.add("imageClass");
+        iconRemove.setAttribute("src", "css/pic/remove.png");
+        linkIconRemove.setAttribute("href", "#");
+        messageBox.appendChild(linkIconRemove);
+        linkIconRemove.appendChild(iconRemove);
+        
     },
     
     eventButtonPressed: function(e){
@@ -32,7 +47,15 @@ var MessageBoard = {
         var messageObject = new Message(node.msg.value, new Date());
         MessageBoard.message.push(messageObject);
         MessageBoard.renderMessage(MessageBoard.message.length - 1);
+        node.msg.value = "";
         e.preventDefault();
+    },
+    eventCloseButtonPressed: function(e){
+        MessageBoard.removeMessage(e.target.parentNode.parentNode.getAttribute("MB-messageID"));
+    },
+    removeMessage: function(messageID){
+        MessageBoard.message.splice(messageID, 1);
+        MessageBoard.renderMessages();
     }
 };
 
