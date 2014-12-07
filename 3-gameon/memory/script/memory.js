af75137fba51b40a13a71bd55f33950f58a43c86
ss223ck <ss223ck@student.lnu.se>
2014-12-07 12:01:32 +0000

    added some more code and started with function generateTable

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -6,7 +6,37 @@ var memory = {
     
     init:function(){
         memory.brickArray = RandomGenerator.getPictureArray(2,2);
-        console.log(memory.brickArray);
+        
+        memory.generateTable();
+        memory.renderBricks();
+        
+    },
+    
+    generateTable: function(){
+        var nodeGameArea = document.getElementById("gameArea");
+        var gameTable = document.createElement("table");
+        
+        gameTable.setAttribute("id", "tableGame");
+        
+        nodeGameArea.appendChild(gameTable);
+    },
+    
+    renderBricks: function(){
+        var nodeGameArea = document.getElementById("gameArea");
+    
+        for(var i = 0; i < memory.brickArray.length; i+=1){
+            
+            var gameBrick = document.createElement("div");
+            var enfoldTag = document.createElement("a");
+            var iconZero = document.createElement("img");
+            iconZero.setAttribute("src", "memory/pics/0.png");
+            iconZero.setAttribute("href", "#");
+            
+            enfoldTag.appendChild(iconZero);
+            gameBrick.appendChild(enfoldTag);
+            nodeGameArea.appendChild(gameBrick);
+            
+        }
     }
     
 };
