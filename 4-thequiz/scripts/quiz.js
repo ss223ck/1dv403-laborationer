358213e177501d32461fc9e64fdda0caaac46b95
ss223ck <ss223ck@student.lnu.se>
2014-12-10 19:45:50 +0000

    fungerar ska bara lägga till tester och justera css

--- 4-thequiz/scripts/quiz.js
+++ 4-thequiz/scripts/quiz.js
@@ -5,8 +5,6 @@ var quiz = {
     serverName: "http://vhost3.lnu.se:20080/question/1",
     infoFromServer: "",
     
-    
-    
     init: function(){
         
         document.getElementById("getQuestion").addEventListener("click", quiz.tryGetQuestion);
@@ -17,32 +15,55 @@ var quiz = {
     tryGetQuestion: function () {
         var xhr = new XMLHttpRequest();
         
+        xhr.open("GET", quiz.serverName, true);
         xhr.onreadystatechange = function(){
             
-            if(xhr.readyState === 4 && xhr.status === 200){
+            if(xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)){
                 quiz.infoFromServer = JSON.parse(xhr.responseText);
+                quiz.renderMessage();
             }
         };
         
-        xhr.open("GET", quiz.serverName, true);
+        
         xhr.send(null);
         
-        quiz.renderMessage();
         
         return false;
     },
     
     trySendQuestion: function(){
+        var answearTextArea = {"answer": document.getElementById("answearTextArea").value},
+            nodeAnswerFromServer = document.getElementById("answerFromServer"),
+            xhrSend = new XMLHttpRequest();
         
-        return false;
+        xhrSend.open("POST", quiz.serverName, true);
+        xhrSend.onreadystatechange = function(){
+            if(xhrSend.readyState === 4 ){
+                if(xhrSend.status === 200 || xhrSend.status === 304){
+                    quiz.infoFromServer = JSON.parse(xhrSend.responseText);
+                    quiz.serverName = quiz.infoFromServer.nextURL;
+                    
+                    if(quiz.infoFromServer.message === "Correct answer!"){
+                        nodeAnswerFromServer.innerHTML = "Du svarade rätt!";
+                    }
+                    
+                }
+                else if(xhrSend.status === 400){
+                    nodeAnswerFromServer.innerHTML = "Du svarade fel!";
+                }
+            }
+            return false;
+        };
+        
+        xhrSend.setRequestHeader('Content-Type', 'application/json');
+        xhrSend.send(JSON.stringify(answearTextArea));
     },
     
     renderMessage: function () {
-        var node = document.getElementById("questionArea");
-        var addPElement = document.createElement("p");
+        var addToElement = document.getElementById("questionElementInQuestionArea");
         
-        addPElement.innerHTML = quiz.infoFromServer.question;
-        node.appendChild(addPElement);
+        addToElement.innerHTML = quiz.infoFromServer.question;
+        quiz.serverName = quiz.infoFromServer.nextURL;
     }
 };
 
